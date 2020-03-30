ed8599d0eb48ada8b1966f6ea7550ce0d8b67090
Eric Lee <ranye0719@gmail.com>
2020-03-30 21:20:11 +0800

    basic architecture

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
